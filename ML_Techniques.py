0b25ce1538ce47460eb0efa9cd099b5fff8340ba
jemmamariyageorge <83303483+jemma-mg@users.noreply.github.com>
2022-08-09 23:54:35 +0530

    Update ML_Techniques.py

--- ML_Techniques.py
+++ ML_Techniques.py
@@ -1,3 +1,37 @@
 '''Codes for basic ML algorithms using python libraries'''
 
+import scipy
+import numpy as np
+import matplotlib.pyplot as plt
+
+
 #Regression
+#Linear Regression Model
+
+
+#Polynomial Regression
+
+
+
+#Classification
+#Perceptron
+
+
+#KNN
+
+
+#Naive Bayes
+
+
+#SVM - Support Vector Machines
+
+
+
+#Decision Trees
+
+
+#Random Forest
+
+
+#K-Means Clustering
+
